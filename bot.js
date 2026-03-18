7c86931acb0243f50a77ac1c55aefec80e3deaff
Lucas Medeiros <lucasmedbasil@gmail.com>
2026-03-18 12:33:50 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,55 @@
-const tmi = require('tmi.js');
+const tmi = require("tmi.js");
+const fs = require("fs");
+
+// carregar comandos do JSON
+const commandsData = JSON.parse(fs.readFileSync("./commands.json", "utf8"));
 
 const client = new tmi.Client({
-    identity: {
-        username: 'voippbot',
-        password: 'oauth: *** '
-    },
-    channels: ['voipp_']
+  identity: {
+    username: "voippbot",
+    password: "oauth:5zeqf4g6k1szolk29hug94e7daurks",
+  },
+  channels: ["voipp_"],
 });
 
 client.connect();
 
-client.on('connected', (address, port) => {
-    console.log(`Bot conectado em ${address}:${port}`);
+client.on("connected", (address, port) => {
+  console.log(`Bot conectado em ${address}:${port}`);
 });
 
+// mensagem automática
 setInterval(() => {
-    client.say("#voipp_", "🔥 Siga o canal para não perder as lives!");
+  client.say("#voipp_", "🔥 Siga o canal para não perder as lives!");
 }, 500000);
 
-let cooldown = false
+// cooldown global
+let cooldown = false;
 
-client.on('message', async (channel, tags, message, self) => {
-    if (self) return;
+client.on("message", async (channel, tags, message, self) => {
+  if (self) return;
 
-    if (cooldown) return;
+  const command = message.toLowerCase().trim();
 
-    cooldown = true;
+  // 🔎 procurar comando no JSON
+  const cmd = commandsData.comandos.find((c) => c.nome === command);
 
-    setTimeout(() => {
-        cooldown = false;
-    }, 3000); 
+  if (cmd) {
+    let resposta = cmd.resposta;
 
-    message = message.toLowerCase();
+    // 🔄 variáveis dinâmicas
+    resposta = resposta.replace("{user}", tags.username);
 
-    switch (true) {
-    
-        case message === '!oi':
-            client.say(channel, `Olá @${tags.username}!`);
-            break;
-    
-        case message === '!teste':
-            client.say(channel, `Tá funcionando bem @${tags.username}!`);
-            break;
+    client.say(channel, resposta);
+    return;
+  }
 
-        case message === '!setup':
-            client.say(channel, `@${tags.username} Meu hardware é: Procesador Ryzen 5 5600 com Radeon Graphics x12; SSD Kingston 1TB; Placa-Mãe Biostar Group A520MH 3.0.`);
-            break;
-        
-        case message === '!comandos':
-            client.say(channel, `@${tags.username} comandos disponíveis: !oi | !teste | !setup | !comandos | !donate`);
-            break;
-        
-        case message.includes('!donate'):
-            client.say(channel, 'Manda o pix para eu receber mais atualizações!');
-            break;
-    }
+  // ⏱️ cooldown global
+  if (cooldown) return;
+
+  cooldown = true;
+
+  setTimeout(() => {
+    cooldown = false;
+  }, 3000);
 });
